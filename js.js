8215c211c5d988983edf8ecbb485e62639e5ff8f
Адель <adel.nuri3v@yandex.ru>
2023-08-29 17:42:59 +0300

    добавил_смайлик

--- js.js
+++ js.js
@@ -3,5 +3,5 @@ const button = document.querySelector('button');
 
 /* Тут на кнопку навешиваем обрабочик, который ждёт клика и тогда запустит логику */
 button.addEventListener('click', function() {
-	alert('Иду до конца, не останавливаюсь на пол пути')
+	alert('Иду до конца, не останавливаюсь на пол пути=)')
 })
